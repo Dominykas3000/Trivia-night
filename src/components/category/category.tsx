76da18e4511943694e910bdbc1ee6f47caae53a4
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2023-09-20 23:01:16 +0300

    selector customisation

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+"use client";
 import style from './category.module.css'
 import CategoriesJson from './categorySelection.json'
+import Triangle from '../../../public/triangle.svg'
+import Link from 'next/link';
 
 interface Category {
   value: string;
@@ -21,18 +26,16 @@ export default function Category() {
         Select a category that you are interested in !
       </h1>
       <hr className={style.sectionTab} />
+
+      <h2 className={style.categorySelectionLabel}>
+        Categories to choose from:
+      </h2>
       <div className={style.categorySelectionContainer}>
-        <label
-          htmlFor="categorySelection"
-          className={style.categorySelectionLabel}
-        >
-          Categories to choose from:
-        </label>
         <select
           name="categorySelection"
           id="categorySelection"
-          className={style.categorySelection}
-        >
+          className={style.categorySelection}>
+
           {categories.map((category) => (
             <option
               className={style.categorySelectionOption}
@@ -41,8 +44,17 @@ export default function Category() {
               {category.name}
             </option>
           ))}
+
         </select>
+        <div className={style.iconContainer} >
+          <Triangle height={14} width={12} />
+        </div>
       </div>
+      <Link href={'/play'}>
+        <button className={style.startButton}>
+          Start Playing!
+        </button>
+      </Link>
     </section>
   )
 }
